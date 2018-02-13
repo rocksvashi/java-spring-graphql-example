24ca72af9d8fb3e370d3a7aaf1362c1b1abd2e74
RocksVashi <RakeshKrVashisht@Roohis-Mac-mini.local>
2018-02-12 20:35:53 -0600

    API endpoint refactored

--- src/main/java/com/sa/graphql/Application.java
+++ src/main/java/com/sa/graphql/Application.java
@@ -1,12 +1,22 @@
 package com.sa.graphql;
 
+import com.sa.graphql.configuration.GraphqlConfiguration;
+import graphql.GraphQL;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public GraphQL graphQL(@Autowired GraphqlConfiguration graphqlConfiguration) throws IOException {
+        return graphqlConfiguration.createGraphQlObject();
     }
 }
--- src/main/java/com/sa/graphql/controllers/Api.java
+++ src/main/java/com/sa/graphql/controllers/Api.java
@@ -1,6 +1,5 @@
 package com.sa.graphql.controllers;
 
-import com.sa.graphql.configuration.GraphqlConfiguration;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-public class MainController {
+public class Api {
 
     private GraphQL graphQL;
-    private GraphqlConfiguration graphQlUtility;
 
     @Autowired
-    MainController(GraphqlConfiguration graphQlUtility) throws IOException {
-        this.graphQlUtility = graphQlUtility;
-        graphQL = graphQlUtility.createGraphQlObject();
+    Api(GraphQL graphQL) throws IOException {
+        this.graphQL = graphQL;
     }
 
     @PostMapping(value = "/query")
-    public ResponseEntity query(@RequestBody String query){
+    public ResponseEntity query(@RequestBody String query) {
         ExecutionResult result = graphQL.execute(query);
-        System.out.println("errors: "+result.getErrors());
+        System.out.println("errors: " + result.getErrors());
         return ResponseEntity.ok(result.getData());
     }
 
     @PostMapping(value = "/mutation")
-    public ResponseEntity mutation(@RequestBody String query){
+    public ResponseEntity mutation(@RequestBody String query) {
         ExecutionResult result = graphQL.execute(query);
-        System.out.println("errors: "+result.getErrors());
+        System.out.println("errors: " + result.getErrors());
         return ResponseEntity.ok(result.getData());
     }
 }
